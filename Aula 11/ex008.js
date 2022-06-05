5980fa038300495ff4a61dd83b2458412cbdecbe
Matheus Alves dos Santos <matheus_santos_7@hotmail.com>
2022-06-05 12:32:02 -0300

    Initial commit

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+  // condição Simples
+  console.log(`Você ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`Diriga sempre usando o sinto de segurança! `)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Italia'
+console.log(`Vivendo em ${país}`)
+if (país != -'Brasil') {
+  console.log(`Você é Brasileiro`)
+} else {
+  console.log(`Você é Estrageira`)
+}
